5ac6d6ba203e530a2a80e2bf55aa9b8ae03452c7
electronic0 <pl259647@gmail.com>
2025-06-21 12:17:21 -0300

    Exercicio 1016

---
+++ 1 - Iniciante/1016 - Distância/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int distancia, tempo;
+
+        distancia = entrada.nextInt();
+
+        tempo = distancia * 2;
+
+        System.out.println(tempo + " minutos");
+
+        entrada.close();
+    }
+}
