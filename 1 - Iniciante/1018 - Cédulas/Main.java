13edba0ecdcf784461f48ba0983f4746db9e8d33
electronic0 <pl259647@gmail.com>
2025-06-22 14:22:56 -0300

    Solução 1018 atualizada

--- 1 - Iniciante/1018 - Cédulas/Main.java
+++ 1 - Iniciante/1018 - Cédulas/Main.java
@@ -4,38 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        int valor, valorLido, cem, cinquenta, vinte, dez, cinco, dois;
+        int [] notas = {100, 50, 20, 10, 5, 2, 1};
+        int valor, valorLido;
 
         valor = entrada.nextInt();
-
         valorLido = valor;
 
-        cem = valor / 100;
-        valor -= cem * 100;
-
-        cinquenta = valor / 50;
-        valor -= cinquenta * 50;
-
-        vinte = valor / 20;
-        valor -= vinte * 20;
-
-        dez = valor / 10;
-        valor -= dez * 10;
-
-        cinco = valor / 5;
-        valor -= cinco * 5;
-
-        dois = valor / 2;
-        valor -= dois * 2;
-
         System.out.println(valorLido);
-        System.out.println(cem + " nota(s) de R$ 100,00");
-        System.out.println(cinquenta + " nota(s) de R$ 50,00");
-        System.out.println(vinte + " nota(s) de R$ 20,00");
-        System.out.println(dez + " nota(s) de R$ 10,00");
-        System.out.println(cinco + " nota(s) de R$ 5,00");
-        System.out.println(dois + " nota(s) de R$ 2,00");
-        System.out.println(valor + " nota(s) de R$ 1,00");
+
+        for (int nota: notas) {
+            System.out.printf("%d nota(s) de R$ %d,00\n", valor / nota, nota);
+            valor %= nota;
+        }
 
         entrada.close();
     }
